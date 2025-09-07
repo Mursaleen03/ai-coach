83402f1fd493a3b7d2f327e24a76fd330b22ee9e
Mursaleen <mohdmursaleen9306@gmail.com>
2025-09-07 22:51:44 +0530

    Some work in left in page.jsx "How It Works " has finished

---
+++ app/page.jsx
@@ -0,0 +1,86 @@
+import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data/features";
+import { howItWorks } from "@/data/howItWorks";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid-background"></div>
+
+
+      <HeroSection />
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">Powerful features for your Career Growth</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {features.map((feature, index) => {
+              return (
+                <Card key={index} className="feature-card border-2 hover:border-primary transition-border duration-300">
+                  <CardContent className={"flex flex-col items-center text-center"}>
+                    <div className="flex flex-col items-center justify-center">
+                      {feature.icon}
+                      <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
+                      <p className="text-muted-foreground">{feature.description}</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+
+
+       <section className="w-full py-12 md:py-24 bg-muted/50">
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">ATS</h3>
+              <p className="text-muted-foreground">-Optimised Resume Builder</p>
+            </div>
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">99.9%</h3>
+              <p className="text-muted-foreground">Server Uptime</p>
+            </div>
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">95%</h3>
+              <p className="text-muted-foreground">Success Rate</p>
+            </div>
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">24/7</h3>
+              <p className="text-muted-foreground">AI Support</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="text-center max-w-3xl mx-auto mb-12">
+          <h2 className="text-3xl font-bold mb-4">
+            How It Works
+          </h2>
+          <p className="text-muted-foreground">Four simple steps to accelerate your Learning</p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {howItWorks.map((item, index) => {
+              return (
+                <div key={index} className="flex flex-col text-center items-center space-y-4">
+                  <div className="w-16 h-16 rounded-b-full bg-primary/10 flex items-center justify-center">
+                    {item.icon}
+                  </div>
+                  <h3 className="text-xl font-semibold">{item.title}</h3>
+                  <p className="text-muted-foreground">{item.description}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
